d26e31bcaaa1edea0ecfe2c97ef44a2ea1573e81
RyanShImprobable <liangren@improbable.io>
2020-04-26 11:16:29 +0800

    more changes

--- WorkerFlagScript/Program.cs
+++ WorkerFlagScript/Program.cs
@@ -1,41 +1,60 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Improbable.SpatialOS.Deployment.V1Alpha1;
 
 namespace WorkerFlagScript
 {
+
+    
     class Program
     {
         private static readonly DeploymentServiceClient DeploymentServiceClient = DeploymentServiceClient.Create();
         static void Main(string[] args)
         {
-            string customer_name = "Nostos";
-            Boolean isWorkflagUsed = false;
-            string[] projects = { "beta_sad_connecticut_304" };
-            foreach (var projectName in projects)
+
+            //string customer_name = "Nostos";         
+            //string[] projects = { "beta_sad_connecticut_304" };
+
+            Dictionary<string, List<string>> customer_projects = new Dictionary<string, List<string>>()
             {
-                Console.WriteLine("Hello World!");
-                var listDeploymentsRequest = new ListDeploymentsRequest
+                { "Ryan", new List<string> { "beta_sad_connecticut_304" } },
+                { "NetEase", new List<string> {"nostos_sa_testing"} },
+
+            };
+            Console.WriteLine("GO and check!");
+            foreach (KeyValuePair<string, List<string>> entry in customer_projects)
+            {
+                
+                Boolean isWorkflagUsed = false;
+                var customer_name = entry.Key;
+                Console.WriteLine(customer_name);
+                foreach (var projectName in entry.Value)
                 {
-                    ProjectName = projectName,
-                    View = ViewType.Full,
-                };
-                var deployments = DeploymentServiceClient.ListDeployments(listDeploymentsRequest);
-                foreach (var deployment in deployments)
-                {
-                   
-                    int count = deployment.WorkerFlags.Count;
-                    if (count > 0)
+                    Console.WriteLine(projectName);
+                    var listDeploymentsRequest = new ListDeploymentsRequest
                     {
-                        isWorkflagUsed = true;
-                        break;
+                        ProjectName = projectName,
+                        View = ViewType.Full,
+                    };
+                    var deployments = DeploymentServiceClient.ListDeployments(listDeploymentsRequest);
+                    foreach (var deployment in deployments)
+                    {
+                        int count = deployment.WorkerFlags.Count;
+                        if (count > 0)
+                        {
+                            isWorkflagUsed = true;
+                            break;
+                        }
+                    }
+
+                    if (isWorkflagUsed)
+                    {
+                        Console.WriteLine("Customer: {0} is using workflag.", projectName);
                     }
                 }
-                deployments.
-                if (isWorkflagUsed)
-                {
-
-                }
             }
+            
             
 
 
